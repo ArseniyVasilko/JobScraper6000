914b749a0c6f12c3d34152b69a81f3ef7692af38
Arseniy <axv409@student.bham.ac.uk>
2026-04-28 22:42:43 +0200

    add get_info() to routes.py

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,10 @@ from flask import render_template
 def results_panel():
 
     return render_template('results_panel.html')
+
+
+@app.route('/get_info', methods=['GET'])
+def get_info():
+    for job_board in request.args.getlist('job_boards'):
+        print("Requesting information for " + job_board)
+        
