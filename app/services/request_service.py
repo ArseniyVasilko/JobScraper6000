4061f48bb148af6be2ac2d40beaa072ccb41def5
Arseniy <axv409@student.bham.ac.uk>
2026-04-30 18:42:49 +0200

    Implement scan_all_jobs() and scan_duunitori() in request_service.py. scan_duunitori currently hardcoded to return first 10 pages (until implement to cut off based on job posting date).

--- app/services/request_service.py
+++ app/services/request_service.py
@@ -1,7 +1,24 @@
+import requests
+from app.constants import ALL_JOB_PAGES
+import time
+import random
 
+#To avoid bot detection
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+}
 
+#Redirects to correct individual job board request method
 def scan_all_jobs(job_board: str) -> list:
-    print("success")
-    return [0, 1]
+    if job_board == "Duunitori":
+        return scan_duunitori()
 
-scan_all_jobs("a")
+#Scans 10 first pages of Duunitori IT industry job listings
+def scan_duunitori() -> list:
+    all_job_bodies = []
+    for i in range(1, 11):
+        response = requests.get(ALL_JOB_PAGES["Duunitori"] + str(i), headers=HEADERS, timeout=20)
+        response.raise_for_status()
+        all_job_bodies.append(response.text)
+        time.sleep(random.uniform(3, 8))
+    return all_job_bodies
