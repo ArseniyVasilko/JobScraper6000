dab96ed1e92fade22c5f0da64ac36b2c05d9a061
Arseniy <axv409@student.bham.ac.uk>
2026-05-07 20:34:06 +0200

    Add print("genai client loaded") to __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -11,5 +11,6 @@ print("config updated")
 #Google genai setup and config
 MODEL = "gemini-2.5-flash-lite"
 client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"])
+print("genai client loaded")
 
 from app import routes
