340680c26a3e48bd626d46c3c82e753d8b5622ec
Arseniy <axv409@student.bham.ac.uk>
2026-05-07 20:30:30 +0200

    Set up initial genai client imports and model name in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from app.config import Config
+import os
+from google import genai
 
+#Flask setup and config
 app = Flask(__name__)
 app.config.from_object(Config)
 print("config updated")
+
+#Google genai setup and config
+MODEL = "gemini-2.5-flash-lite"
+client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"])
 
 from app import routes
