6a87c9390ab40dd7e9562e3eb5064d185b512505
Arseniy <axv409@student.bham.ac.uk>
2026-05-22 15:32:44 +0200

    Fix SECRET_KEY duplicate in config.py

--- app/config.py
+++ app/config.py
@@ -3,7 +3,6 @@ from datetime import timedelta
 
 class Config:
     DEBUG = True
-    SECRET_KEY = "my-secret"
     SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
